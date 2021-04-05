0fb4f23aad7fb17f8df46941dc6f8d7e0ba0bbac
Rick Brown <ricky.brown.00@gmail.com>
2021-04-05 22:02:54 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Rick Brown on 05/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
